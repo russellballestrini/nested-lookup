1326642f5fce49b2d0d6aa3ea615a815cd045b39
Russell Ballestrini <russell.ballestrini@gmail.com>
2018-11-02 12:09:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt', 'r') as f:
 
 setup( 
     name = 'nested-lookup',
-    version = '0.1.8',
+    version = '0.2.01',
     description = 'lookup a key in a deeply nested document of dicts and lists',
     keywords = 'nested document dictionary dict list lookup schema json xml yaml',
     long_description = open('README.rst').read(),
