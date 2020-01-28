2e8098ef622c6f69d8971f35c90cad7edbb48d25
Matheus Lins <matheus.lins@br.experian.com>
2020-01-28 15:38:07 -0300

    Add a new method to return count of values and self values

--- nested_lookup/__init__.py
+++ nested_lookup/__init__.py
@@ -3,5 +3,6 @@ from .nested_lookup import (
     get_all_keys,
     get_occurrence_of_key,
     get_occurrence_of_value,
+    get_occurrence_of_value_with_self_value
 )
 from .lookup_api import nested_update, nested_delete, nested_alter
